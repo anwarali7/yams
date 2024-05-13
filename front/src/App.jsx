9115f43d8392908df816d5b8f99768067bb56de2
MarinaSigida <sigida.web@gmail.com>
2024-05-13 17:39:02 +0200

    routing
    feat: Add routing functionality using React Router
    
    - Install React Router dependencies
    - Set up routes for different pages/components
    - Implement navigation using NavLink and Route components
    - Create a ShareLayout component to contain the navigation and main content
    - Ensure proper rendering and navigation between pages

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Game from './pages/Game';
+import Results from './pages/Results';
+import LogIn from './pages/LogIn';
+import SharedLayout from './components/SharedLayout';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="game" element={<Game />} />
+          <Route path="results" element={<Results />} />
+          <Route path="login" element={<LogIn />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
