d5964880ef23b54d9e41341f52cc76cccd787a07
MarinaSigida <sigida.web@gmail.com>
2024-05-14 17:00:06 +0200

    add Home page
    1. Add welcome text
    2. Add prize options
    3. Add button to go to the Game page
    4. Add link to navigate to Rules page (not presented in the NavBar)

--- front/src/components/SharedLayout.jsx
+++ front/src/components/SharedLayout.jsx
@@ -1,26 +1,33 @@
 import { Outlet, NavLink } from 'react-router-dom';
 import logo from '../images/rolling-dices-svgrepo-com.svg';
 import './SharedLayout.css';
+import user from '../images/user.png';
 
 const SharedLayout = () => {
   return (
     <div>
       <nav className="nav-container">
-        <img src={logo} alt="logo" width="40" height="40" />
+        <div className="logo">
+          <img src={logo} alt="logo" width="40" height="40" />
+          <h2>Yams</h2>
+        </div>
         <div className="nav-bar">
           <NavLink to="/" end className="navLink">
-            Home
+            Accueil
           </NavLink>
           <NavLink to="/game" className="navLink">
-            Game
+            Jeu
           </NavLink>
           <NavLink to="/results" className="navLink">
-            Results
+            Résultats
           </NavLink>
         </div>
-        <NavLink to="/login" className="navLink">
-          Log in
-        </NavLink>
+        <div className="connection">
+          <img src={user} alt="user-icon" width="20" height="20" />
+          <NavLink to="/login" className="navLink">
+            Se connecter
+          </NavLink>
+        </div>
       </nav>
 
       <div className="main-content">
