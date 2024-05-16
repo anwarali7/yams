b88ed02dc4165d16117acf2eda97704722bd0ecc
MarinaSigida <sigida.web@gmail.com>
2024-05-16 14:23:39 +0200

    pastries-request

--- front/src/components/PastryList.jsx
+++ front/src/components/PastryList.jsx
@@ -1,7 +1,27 @@
+import {
+  useGetAllPastriesQuery,
+  useAddOnePastryMutation,
+  useDeletePastryByIdMutation,
+} from 'src/features/pastries-api/pastriesApi.jsx';
+
 import PastryItem from './PastryItem';
 import './PastryList.css';
 
 const PastryList = () => {
+  const { data, error, isLoading } = useGetAllPastriesQuery();
+
+  if (isLoading) {
+    return <div className="message">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="message">{error.message}</div>;
+  }
+
+  if (!data || data.length === 0) {
+    return <div className="message">Pas de pâtisseries disponibles</div>;
+  }
+
   return (
     <div className="pastry-list">
       <p>Nombre de pâtisseries : 30</p>
@@ -15,14 +35,7 @@ const PastryList = () => {
         <button className="search-box-button">&#128269;</button>
       </div>
       <div className="pastries-wrapper">
-        <PastryItem />
-        <PastryItem />
-        <PastryItem />
-        <PastryItem />
-        <PastryItem />
-        <PastryItem />
-        <PastryItem />
-        <PastryItem />
+        <PastryItem data={data} />
       </div>
     </div>
   );
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,15 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { yamsApi } from 'src/features/yams-api/yamsApi.jsx';
+import { pastriesApi } from '../features/pastries-api/pastriesApi';
 import pastriesWonReducer from '../features/pastriesWon/pastriesWonSlice';
 
 const store = configureStore({
   reducer: {
     pastriesWon: pastriesWonReducer,
     [yamsApi.reducerPath]: yamsApi.reducer,
+    [pastriesApi.reducerPath]: pastriesApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(yamsApi.middleware),
+    getDefaultMiddleware().concat(yamsApi.middleware, pastriesApi.middleware),
 });
 
 setupListeners(store.dispatch);
