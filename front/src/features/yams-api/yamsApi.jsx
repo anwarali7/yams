882555048981bee5ed37c20af0353241d7653dc6
anwarali7 <ali.anwar75751@gmail.com>
2024-05-17 12:48:38 +0200

    chore: Update yamsApi endpoints and remove unused code
    Co-authored-by: MarinaSigida <MarinaSigida@users.noreply.github.com>

--- front/src/features/yams-api/yamsApi.jsx
+++ front/src/features/yams-api/yamsApi.jsx
@@ -5,14 +5,12 @@ const urlApi = 'http://localhost:3001';
 export const yamsApi = createApi({
   reducerPath: 'yamsApi',
   baseQuery: fetchBaseQuery({ baseUrl: urlApi }),
-  // tagTypes: ['Pastries'],
   endpoints: (builder) => ({
     getAllPastries: builder.query({
       query: () => `game/pastries`,
     }),
     winPastries: builder.query({
       query: (quantity) => `game/win-pastries/${quantity}`,
-      // invalidatesTags: ['Pastries'],
     }),
   }),
 });
